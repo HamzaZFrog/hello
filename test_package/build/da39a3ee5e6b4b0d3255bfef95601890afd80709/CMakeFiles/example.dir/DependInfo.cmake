6cd203a379f0939e86c724fb9a1ab8407021f471
totoro <hamzaz@jfrog.com>
2017-09-09 19:39:37 +0200

    first commit

---
+++ test_package/build/da39a3ee5e6b4b0d3255bfef95601890afd80709/CMakeFiles/example.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hamza/conan-demo/travis-ci/hello/test_package/example.cpp" "/home/hamza/conan-demo/travis-ci/hello/test_package/build/da39a3ee5e6b4b0d3255bfef95601890afd80709/CMakeFiles/example.dir/example.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_GLIBCXX_USE_CXX11_ABI=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/hamza/.conan/data/Hello/0.1/hello/hamzaz/package/75afd4a5a43dc297f65bd5338e9887b268cb97ba/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
